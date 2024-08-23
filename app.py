9f49992877a7e53cd3497966124980314a246b4e
hariharan7089 <hariharank007089@gmail.com>
2024-08-23 11:51:37 +0530

    new

--- app.py
+++ app.py
@@ -1,22 +1,8 @@
-from flask import Flask
-from extension import mongo
-from main.routes import main
+from flask import Flask,render_template,redirect
 
-def create_app():
-    app = Flask(__name__)
+app=Flask(__name__)
 
-    # MongoDB URI - replace with your actual MongoDB URI
-    app.config["MONGO_URI"]  ="mongodb+srv://hariharank:HARIHARANK@cluster0.7njml.mongodb.net/todo?tls=true&authSource=website&authMechanism=SCRAM-SHA-1"
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-    
-    # Initialize PyMongo with the Flask app
-    mongo.init_app(app)
-
-    # Register blueprint
-    app.register_blueprint(main)
-    
-    return app
-
-if __name__ == "__main__":
-    app = create_app()
-    app.run(port=5000 ,debug=True)
